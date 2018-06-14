3aed973687b2603c4a251ce5dd352e6b5f2ab235
Paven N <novolodskiy.p@gmail.com>
2018-06-14 22:18:25 +0300

    try to add codecov

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,24 @@
 <template>
+  <!--<div id="app">-->
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
+  <!--</div>-->
+
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
 export default {
   name: 'App'
 }
