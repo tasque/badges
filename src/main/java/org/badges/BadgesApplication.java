77a96870b477cfbb190266e5aa73d04d218a389c
Paven N <novolodskiy.p@gmail.com>
2018-10-12 22:11:52 +0300

    try to add quartz

--- src/main/java/org/badges/BadgesApplication.java
+++ src/main/java/org/badges/BadgesApplication.java
@@ -2,9 +2,11 @@ package org.badges;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class BadgesApplication {
 
     public static void main(String[] args) {
