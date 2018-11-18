ea84c9ae6ac50d95c22079abf840476c398ee8a2
Pavel Novolodskiy <pnovolodskiy@marathonbet.ru>
2018-11-18 17:39:47 +0300

    add base method to upload users

--- src/main/java/org/badges/api/controller/admin/UsersAdminController.java
+++ src/main/java/org/badges/api/controller/admin/UsersAdminController.java
@@ -1,6 +1,9 @@
 package org.badges.api.controller.admin;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.badges.api.domain.UserDto;
 import org.badges.api.domain.admin.AdminUser;
 import org.badges.db.User;
@@ -10,15 +13,26 @@ import org.badges.security.annotation.RequiredPermission;
 import org.badges.service.converter.UserConverter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin/users")
+@Slf4j
 public class UsersAdminController {
 
     private final UserRepository userRepository;
@@ -39,5 +53,21 @@ public class UsersAdminController {
         return new AdminUser()
                 .setId(user.getId())
                 .setName(user.getName());
+    }
+
+    @PostMapping("/uploadUsers")
+    @Transactional
+    public Collection<Object> uploadUsers(MultipartFile file) throws IOException {
+        Map<String, User> usersByEmail = userRepository.findAll().stream()
+                .collect(Collectors.toMap(User::getEmail, Function.identity()));
+
+        XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        sheet.forEach(row -> {
+            String email = row.getCell(1).getStringCellValue();
+            log.info(email + usersByEmail.get(email) + row);
+        });
+
+        return Collections.emptyList();
     }
 }
