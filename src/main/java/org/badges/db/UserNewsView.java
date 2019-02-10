407321f4cbc3667d8ab7d0d1b466b79f91eaf039
Pavel Novolodskiy <pnovolodskiy@marathonbet.ru>
2019-02-10 19:43:55 +0300

    forget id

--- src/main/java/org/badges/db/UserNewsView.java
+++ src/main/java/org/badges/db/UserNewsView.java
@@ -7,6 +7,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
@@ -15,6 +18,9 @@ import javax.persistence.Entity;
 @Entity
 public class UserNewsView {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long userId;
     private Long entityId;
