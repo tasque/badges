b956121e9367147f3079daeb0a07dad43a225256
Pavel Novolodskiy <pnovolodskiy@marathonbet.ru>
2019-03-20 17:25:57 +0300

    remove useless bracets

--- src/main/java/org/badges/db/repository/UserNewsViewRepository.java
+++ src/main/java/org/badges/db/repository/UserNewsViewRepository.java
@@ -15,7 +15,7 @@ public interface UserNewsViewRepository extends JpaRepository<UserNewsViews, Lon
     @Modifying
     @Query(value = "INSERT INTO public.user_news_views (user_id, entity_id, event_type) " +
             "VALUES (:userId, :entityId, :eventType) " +
-            "ON CONFLICT ON CONSTRAINT (unique_unw_user_entity_event) " +
+            "ON CONFLICT ON CONSTRAINT unique_unw_user_entity_event " +
             "   DO update set count_of_views = user_news_views.count_of_views + 1", nativeQuery = true)
     void saveQuietly(@Param("userId") Long userId, @Param("entityId") Long entityId, @Param("eventType") String eventType);
 
